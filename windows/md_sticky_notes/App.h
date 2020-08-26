196defcacc5be320caaae34615b6a49b9bfcc427
Esteban Munoz <esteban.230@hotmail.com>
2020-08-26 15:41:54 -0600

    initial commit

---
+++ windows/md_sticky_notes/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::md_sticky_notes::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::md_sticky_notes::implementation
+
+
---
+++ windows/md_sticky_notes/ReactPackageProvider.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "winrt/Microsoft.ReactNative.h"
+
+
+namespace winrt::md_sticky_notes::implementation
+{
+    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
+    {
+    public: // IReactPackageProvider
+        void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
+    };
+} // namespace winrt::md_sticky_notes::implementation
+
+
