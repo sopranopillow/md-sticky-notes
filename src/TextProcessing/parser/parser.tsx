7463b493d843f66c73f9016cedbb5deb4ffe26c6
Esteban Munoz <esteban.230@hotmail.com>
2020-11-18 20:46:44 -0700

    React native comes with tons of issues :/ will switch to electron

---
+++ src/TextProcessing/parser/parser.tsx
@@ -0,0 +1,8 @@
+// import * as rules from '../LanguageRules/MDRules.json';
+
+const parseText = (text: string) => {
+    // check for regex but save content, remove content and start.end then insert styled content.
+    return '';
+}
+
+export default parseText;
